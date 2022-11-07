5c593c67ea7736c5ed87ae4e39e19e331b818dcd
Ursulino Costa <71571163+Ursulinocosta@users.noreply.github.com>
2022-11-07 19:45:02 -0300

    Add files via upload

---
+++ Pokemon2/src/pokedex/Pokemon2.java
@@ -0,0 +1,17 @@
+
+package pokedex;
+
+/**
+ *
+ * @author ursulino
+ */
+public class Pokemon2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
